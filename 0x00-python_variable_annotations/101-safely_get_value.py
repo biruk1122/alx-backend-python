b34872b60a2a1dd457ef896eded0221005aac743
biruk1122 <birukfikadu29@gmail.com>
2024-05-02 02:57:01 -0700

    updated

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-from typing import TypeVar, Dict, Any, Optional
+from typing import TypeVar, Mapping, Any, Union
 
 K = TypeVar('K')  # Type variable for the dictionary key
 V = TypeVar('V')  # Type variable for the dictionary value
 
-def safely_get_value(dct: Dict[K, V], key: K, default: Optional[V] = None) -> Optional[V]:
+def safely_get_value(dct: Mapping[K, V], key: K, default: Union[V, None] = None) -> Union[V, None]:
     if key in dct:
         return dct[key]
     else:
